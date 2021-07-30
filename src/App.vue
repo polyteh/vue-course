f948a1d01f3361ded08b734244827f4727ba4a9e
Ivan_Prevo <ivan.prevo@ideo-digital.com>
2021-07-30 12:12:05 +0300

    add base post logic

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app-holder">
+    Base App
+    <div class="add-post">
+      <form class="add-post-form" @submit.prevent>
+        <h4>Create new post</h4>
+        <input class="create-post-input" placeholder="Enter post title" v-bind:value="title" type="text" @input="title=$event.target.value">
+        <input class="create-post-input" placeholder="Enter post description" v-bind:value="description" type="text" @input="inputDescription">
+        <button class="create-post-btn" @click="createPost">Add post</button>
+      </form>
+    </div>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>{{post.title}}</strong> {{post.description}}</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default{
+  data(){
+    return {
+      posts: [
+        {id: 1, title: "Olymplic Games 2020", description: "Olymplic Games 2020 was postponed to 2021"},
+        {id: 2, title: "F1 2021", description: "The laterst news from F1 world"},
+        {id: 3, title: "Champoins League 2021/2022", description: "Cooming soon"},
+      ],
+      title: '',
+      description: ''
+    }
+  },
+  methods:{
+    createPost(){
+        const newPost={
+          id: Date.now(),
+          title: this.title,
+          description: this.description
+        };
+        this.posts.push(newPost);
+    },
+    inputTitle(event){
+      this.title = event.target.value;
+    },
+    inputDescription(event){
+      this.description = event.target.value;
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+.post{
+  padding: 15px;
+  border: 1px solid steelblue;
+  margin-top: 15px;
+}
+.create-post-input{
+    border: 1px solid steelblue;
+    width: 100%;
+    padding: 10px;
+    margin-top: 15px;
+}
+.app-holder{
+  padding: 20px;
 }
 
-#nav {
-  padding: 30px;
+.add-post-form{
+  display: flex;
+  flex-direction: column;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.create-post-btn{
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px;
+  border: 1px solid steelblue;
+  background: none;
+  color: steelblue;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
